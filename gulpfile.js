6d2eebbc0afd831b2744533a48a4164100cb09ad
Jane Wilson <janeawilson1990@gmail.com>
2019-07-04 22:30:05 +0100

    initial commit
    I am a bad person who does git wrong and gets quite deep into actually making the thing before doing anything with version control

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+	
+var paths = {
+    styles: {
+        src: "./scss/**/*.scss",
+        // Compiled files will end up in whichever folder it's found in (partials are not compiled)
+        dest: "./"
+    }
+};
+
+function style() {
+    return gulp
+        .src(paths.styles.src)
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(gulp.dest(paths.styles.dest));
+}
+ 
+function watch() {
+    //I usually run the compile task when the watch task starts as well
+    style();
+ 
+    gulp.watch(paths.styles.src, style);
+}
+
+// Don't forget to expose the task!
+exports.watch = watch
